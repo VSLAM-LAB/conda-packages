39890f4801b5e80f8637b8609791d01e82ca24e5
alejandrofontan <fontanvillacampa@gmail.com>
2026-01-13 16:32:57 +1000

    brisk

---
+++ BRISK-VSLAM-LAB/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+mkdir build
+cd build
+
+cmake .. \
+      -G "Ninja" \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_PREFIX_PATH=$PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX 
+
+cmake --build . --config Release
+ninja install -j${CPU_COUNT}
